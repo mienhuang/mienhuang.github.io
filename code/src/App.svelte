ffe7f0b3196fbe93ec3c094205eaac120ace6360
mien <huangxiaomeng_vip@126.com>
2020-03-21 10:38:48 +0800

    update

--- code/src/App.svelte
+++ code/src/App.svelte
@@ -1,29 +1,39 @@
 <script>
-	export let name;
+  import Dashboard from "./Dashboard.svelte";
+//   export let name = "test";
+  let test = "123";
+  function update() {
+	  console.log('called', updatevalue)
+    updatevalue();
+  }
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-</main>
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
+
+<main>
+<h1>See you soon!</h1>
+  <!-- <h1>Hello {name}!</h1>
+  <button on:click={update}>Update</button>
+  <h1>{test}</h1>
+  <Dashboard bind:test={test}/> -->
+</main>
---
+++ code/src/Dashboard.svelte
@@ -0,0 +1,13 @@
+<script context="module" >
+  let totalComponents = 1;
+//   export let test;
+</script>
+
+<style>
+
+</style>
+
+<main>
+  <h1>{totalComponents}</h1>
+  <!-- <span>{test}</span> -->
+</main>
